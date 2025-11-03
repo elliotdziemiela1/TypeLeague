8b974cbb605606533eab54b50d70f59ac8a2f3cb
Your Name <elliotanthony9@gmail.com>
2025-11-03 15:47:26 -0600

    commenting out all backend except for welcome message

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,9 +12,9 @@ interface UserData {
 }
 
 // connect to SupaBase
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
 const app = express();
 
@@ -25,75 +25,75 @@ app.get("/api", (req : Request, res : Response) => {
     res.status(200).send("Hello from the api!");
 })
 
-// Get all users
-app.get("/api/users", async (req : Request, res : Response) =>{
-    const { data, error } = await supabase.from("Users").select("*");
+// // Get all users
+// app.get("/api/users", async (req : Request, res : Response) =>{
+//     const { data, error } = await supabase.from("Users").select("*");
     
-    if (error) {
-        console.log(`get users error: ${error.message}. Details: ${error.details}`);
-        return res.status(500).send();
-    }
+//     if (error) {
+//         console.log(`get users error: ${error.message}. Details: ${error.details}`);
+//         return res.status(500).send();
+//     }
 
-    res.status(200).send(data);
-})
+//     res.status(200).send(data);
+// })
 
-// get specific user
-app.get("api/users/:username", async (req,res) => {
-    const { data, error } = await supabase.from("Users").select("*").eq("username",req.params.username);
+// // get specific user
+// app.get("api/users/:username", async (req,res) => {
+//     const { data, error } = await supabase.from("Users").select("*").eq("username",req.params.username);
 
-    if (error){
-        console.log(`get specific user search error ${error}`);
-        res.status(500).send();
-    }
-    res.status(200).send(data);
-})
+//     if (error){
+//         console.log(`get specific user search error ${error}`);
+//         res.status(500).send();
+//     }
+//     res.status(200).send(data);
+// })
 
-// create new user
-app.post("/api/users", async (req : Request, res : Response) => {
-    const reqJSON = req.body;
-    console.log(reqJSON);
+// // create new user
+// app.post("/api/users", async (req : Request, res : Response) => {
+//     const reqJSON = req.body;
+//     console.log(reqJSON);
     
-    const { data, error } = await supabase.from("Users").insert(req.body);
+//     const { data, error } = await supabase.from("Users").insert(req.body);
 
-    if (error){
-        console.log(`post to users error: ${error}`);
-        res.status(500).send();
-        return;
-    }
+//     if (error){
+//         console.log(`post to users error: ${error}`);
+//         res.status(500).send();
+//         return;
+//     }
 
-    res.status(200).send();
-})
+//     res.status(200).send();
+// })
 
-// update user
-app.put("/api/users/:username", async (req, res) => {
-    const body = req.body as UserData;
-    console.log(`put request with body ${body}`);
+// // update user
+// app.put("/api/users/:username", async (req, res) => {
+//     const body = req.body as UserData;
+//     console.log(`put request with body ${body}`);
 
-    const { data, error } = await supabase.from("Users").update(body).eq("username", req.params.username);
-    // console.log("returned from supabase")
+//     const { data, error } = await supabase.from("Users").update(body).eq("username", req.params.username);
+//     // console.log("returned from supabase")
 
-    if (error){
-        console.log(`put to users error: ${error}`)
-        res.status(500).send();
-        return
+//     if (error){
+//         console.log(`put to users error: ${error}`)
+//         res.status(500).send();
+//         return
 
-    }
+//     }
 
-    res.status(200).send();
-})
+//     res.status(200).send();
+// })
 
-// delete a user
-app.delete("/api/users/:username", async (req, res) => {
-    const { data, error } = await supabase.from("Users").delete().eq("username",req.params.username);
+// // delete a user
+// app.delete("/api/users/:username", async (req, res) => {
+//     const { data, error } = await supabase.from("Users").delete().eq("username",req.params.username);
 
-    if (error){
-        console.log(`error with deleting user: ${error}`);
-        res.status(500).send();
-        return;
-    }
+//     if (error){
+//         console.log(`error with deleting user: ${error}`);
+//         res.status(500).send();
+//         return;
+//     }
 
-    res.status(200).send();
-})
+//     res.status(200).send();
+// })
 
 
 
